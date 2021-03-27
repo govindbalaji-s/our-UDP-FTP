d4113f85112da48e7d7aafed45ace82060950ab2
Govind Balaji S <s.govindbalaji@gmail.com>
2021-03-27 17:28:19 +0530

    Data types SenderState, CongestionState

--- ourudpftp.py
+++ ourudpftp.py
@@ -1,5 +1,18 @@
+V1_CHUNK_SIZE = 504
+
+import threading
+
 def sendto(fname, dest)):
     ip, port = dest
+
+    ## read file 
+    ## init sender's state
+
+    ## handshake on udp
+
+    ## infinite loop based on sender's state:
+        ## receive on udp and call functions to update sender's state
+        ## send on udp based on sender's state
     #
     #
 
@@ -7,3 +20,25 @@ def recv_at(src):
     ip, port = src
     #
     #
+
+class SenderState:
+    
+    def __init__(self, data:bytes)
+        self.data:bytes = data
+        self.chunks = []
+        ## TODO compute chunks
+        ## for i in 
+        self.unacked_chunks:set = set(range(len(self.chunks)))
+        self.congestion:CongestionState = CongestionState()
+        #self.flow = FlowState() TODO add flow control
+        self.timers:list[threading.Timer] = [] 
+
+    def ack_received():
+        # update unacked chunks
+
+class CongestionState:
+    def __init__():
+        self.cwnd:int = 0
+        self.addconst:int = 1
+        self.ssthresh:int = 1
+        self.is_slow_start:bool = True
