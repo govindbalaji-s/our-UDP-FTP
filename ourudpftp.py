7c22eb3809513f7cb0ab5b95fe990139b32c5645
Sumadhva Sridhar <es18btech11018@iith.ac.in>
2021-03-30 11:55:13 +0530

    Receive Data

--- ourudpftp.py
+++ ourudpftp.py
@@ -8,9 +8,8 @@ V1_TYPE_DATA_ACK = 3
 import threading
 import math
 import socket
-from BitVector import BitVector
 
-def sendto(fname, dest)):
+def sendto(fname, dest):
     ip, port = dest
 
     ## read file 
@@ -44,7 +43,7 @@ class Sender:
         self.timeoutval = 0.2
         self.timers:list[threading.Timer] = [] 
 
-    def do_handshake():
+    def do_handshake(self):
         mdata = Metadata(len(self.chunks), self.fname)
         pload = mdata.to_bytes()
         mdpkt = Packet.fromVals(V1_TYPE_MDATA, 0, pload)
@@ -63,9 +62,9 @@ class Sender:
 
             except socket.timeout:
                 continue
-				
-	def send_data():    #func to start sending data, and recieving ACKs for sent data.
-        
+
+    def send_data(self):    #func to start sending data, and recieving ACKs for sent data.
+
         def listen_for_acks():
             while True:
                 data, src = sock.recvfrom(512)
@@ -75,21 +74,26 @@ class Sender:
                     ##lock here?
                     self.unacked_chunks.discard(ackpkt.seqnum)  ##if already acked, then does nothing
                 else:
+                    pass
                     ##Do nothing??
                 if len(self.unacked_chunks) == 0:
                     break
-        
+
         thread_ACK = threading.Thread(target=listen_for_acks)
         thread_ACK.start()
-        
+
         for seq_num in self.unacked_chunks: ##How do we put a lock here? (:
-            utp_pkt = Packet.fromVals(type_ = 1 , seqnum = seq_num, payload = self.chunks[seq_num]) #fromVals takes care of checksumcalculation
+            utp_pkt = Packet(type_ = 1 , seqnum = seq_num, payload = self.chunks[seq_num], checksum = 0)
+            utp_pkt.calc_paylen()
+            test_msg = utp_pkt.to_bytes()   ##test_msg has checksum = 0, and is entirely in bytes
+            utp_pkt.calc_checksum()         ##Checksum computed and updated
+
             sock = socket.socket(socket.AF_INET, sock.SOCK_DGRAM)
             sock.bind(self.myaddr)
             sock.sendto(utp_pkt.to_bytes(), self.dest)
             ##Ig some sort of timeout here before sending again?
-			
-		thread_ACK.join()
+
+        thread_ACK.join()
 
 
     # Breaks self.data into self.chunks
@@ -105,6 +109,7 @@ class Sender:
 
 
     def ack_received(self):
+        pass
         # update unacked chunks
 
 class CongestionState:
@@ -141,18 +146,17 @@ class Metadata :
     def tobytes(self) -> bytes:
         return self.numchunks.to_bytes(4, 'big') + self.filename.to_bytes(500, 'big')
 
-class Receiverstate :
-    def __init__(self, metadata,myaddr:(str, int)):
-        self.metadata = metadata
+class Receiver:
+    def __init__(self, myaddr:(str, int)):
+        self.metadata = None
         self.myaddr = myaddr
-        self.dest = dest
         self.chunks = []  
         self.pending_chunks:set = set(range(len(self.chunks)))
-        self.temp_filepath
 
-    def do_handshake():
+    def do_handshake(self):
         sock = socket.socket(socket.AF_INET, sock.SOCK_DGRAM)
         sock.bind(self.myaddr)
+
         while True:
             data, self.src = sock.recvfrom(512)
             Headerpkt = Packet.fromBytes(data)
@@ -161,58 +165,76 @@ class Receiverstate :
                 sck.sendto(ackpt.to_bytes(),self.src)
                 break
 
+        self.metadata = Metadata.fromBytes(Headerpkt.payload)
+        self.chunks = [None for _ in range(self.metadata.numchunks)]  # Initialize list of chunks
+        self.pending_chunks = set(range(self.metadata.numchunks))  # Initialize list of pending chunks
+
+    def receive_data(self):
+        sock = socket.socket(socket.AF_INET, sock.SOCK_DGRAM)
+        sock.bind(self.myaddr)
+
+        while len(pending_chunks) > 0:  # While chunks are still pending
+            data, self.src = sock.recvfrom(512)
+            pkt = Packet.fromBytes(data)
+
+            if pkt.verify_checksum() and pkt.type_ == V1_TYPE_MDATA:  # If metadata, send mdack
+                ackpt = Packet.fromVals(V1_TYPE_MDATA_ACK, pkt.seqnum, int(0).to_bytes(0,'big'))
+                sck.sendto(ackpt.to_bytes(),self.src)
+
+            elif pkt.verify_checksum() and pkt.type_ == V1_TYPE_DATA:  # If chunk, store chunk and update pending chunks
+                seqnum = pkt.seqnum
+                self.chunks[seqnum] = Chunk(pkt.payload, seqnum)
+                self.pending_chunks.remove(seqnum)
+
+
 class Packet :
-	def __init__(self, type:int, seqnum:int, payload, payload_length:int=None, checksum:int=None):
-		self.version = 1
-		self.type_ = type_
-		self.payload_length = payload_length
-		self.checksum = checksum
-		self.seqnum = seqnum
-		self.payload = payload
+    def __init__(self, type:int, seqnum:int, payload, payload_length:int=None, checksum:int=None):
+        self.version = 1
+        self.type_ = type_
+        self.payload_length = payload_length
+        self.checksum = checksum
+        self.seqnum = seqnum
+        self.payload = payload
 
-	@classmethod
-	def fromVals(cls, type_:int, seqnum:int, payload):  ## Creating Packet at Sender
-		pkt = cls(type_=type_, seqnum=seqnum, payload=payload)
-		pkt.calc_paylen()
-        test_msg = pkt.to_bytes()
-		pkt.checksum = calc_checksum(test_msg)
-		return pkt
+    @classmethod
+    def fromVals(cls, type_:int, seqnum:int, payload):  ## Creating Packet at Sender
+        pkt = cls(type_=type_, seqnum=seqnum, payload=payload)
+        pkt.calc_paylen()
+        pkt.calc_checksum()
+        return pkt
 
-	@classmethod
-	def fromBytes(cls, data):  ## Creating packet at Receiver
-		version = data[0] >> 4
-		type_ = (data[0] >> 2) & 3
-		payload_length = ((data[0] & 3) << 8) + data[1]
-		checksum = int(data[2:4])
-		seqnum = int(data[4:8])
-		payload = data[8:]
-		return cls(type_, seqnum, payload, payload_length, checksum)
+    @classmethod
+    def fromBytes(cls, data):  ## Creating packet at Receiver
+        version = data[0] >> 4
+        type_ = (data[0] >> 2) & 3
+        payload_length = ((data[0] & 3) << 8) + data[1]
+        checksum = int(data[2:4])
+        seqnum = int(data[4:8])
+        payload = data[8:]
+        return cls(type_, seqnum, payload, payload_length, checksum)
 
-	def to_bytes(self):
-		# 4 bits version, 2 bits type, 10 bits payload length
-		## Error handle version, type etc ranges TODO
-		first = (self.version << 28) + (self.type << 26) + 
-			(self.payload_length << 16) + self.checksum
+    def to_bytes(self):
+        # 4 bits version, 2 bits type, 10 bits payload length
+        ## Error handle version, type etc ranges TODO
+        first = (self.version << 28) + (self.type << 26) + (self.payload_length << 16) + self.checksum
 
-		return first.to_bytes(4, 'big') + self.seqnum.to_bytes(4, 'big') + self.payload
+        return first.to_bytes(4, 'big') + self.seqnum.to_bytes(4, 'big') + self.payload
 
-	def calc_paylen(self):
-		self.payload_length = len(self.payload)
+    def calc_paylen(self):
+        self.payload_length = len(self.payload)
 
-#keeping calc_checksum seperate
-def calc_checksum(msg:bytes):
-	#self.checksum = 0
-	## Calculate checksum
-	def add_carry(a, b):
-		c = a + b
-		return (c & 0xffff) + (c >> 16) #carry bit wrapped around
-	s = 0
-	#if len(msg)%2 == 0
-	for i in range(0, len(msg), 2):
-		next_word = (msg[i]<<8) + msg[i+1]
-		s = add_carry(s, next_word)
-	return ~s & 0xffff
-
+    def calc_checksum(self, msg:bytes):
+        #self.checksum = 0
+        ## Calculate checksum
+        def add_carry(a, b):
+            c = a + b
+            return (c & 0xffff) + (c >> 16) #carry bit wrapped around
+        s = 0
+        for i in range(0, len(msg), 2):
+            next_word = (msg[i]<<8) + msg[i+1]
+            s = add_carry(s, next_word)
+        self.checksum =  ~s & 0xffff
+		
 # def calc_checksum(self, msg:bytes):
 # 	#self.checksum = 0
 # 	## Calculate checksum
@@ -225,6 +247,6 @@ def calc_checksum(msg:bytes):
 # 	    s = add_carry(s, next_word)
 # 	self.checksum =  ~s & 0xffff
 
-    def verify_checksum():
+    def verify_checksum(self):
+        pass
         ##
-
