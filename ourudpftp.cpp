c8bffdf3de89cf73faa7388bb6a68b27bbb5cf15
Govind Balaji S <s.govindbalaji@gmail.com>
2021-03-31 16:04:32 +0530

    Ported sender's handshake

--- ourudpftp.cpp
+++ ourudpftp.cpp
@@ -5,6 +5,8 @@
 #include <errno.h>
 #include <string>
 #include <utility>
+#include <vector>
+#include <ifstream>
 
 using namespace std;
 
@@ -14,6 +16,7 @@ const int V1_TYPE_MDATA = 0;
 const int V1_TYPE_DATA = 1;
 const int V1_TYPE_MDATA_ACK = 2;
 const int V1_TYPE_DATA_ACK = 3;
+const string TIMEOUT_IP = "TIMEOUT";
 
 extern int errno;
 
@@ -40,12 +43,12 @@ int setup_std_sock(pair<string, int> myaddr, long timeout=0) {
     return sd;
 }
 
-pair<string, int> std_recvfrom(int sd, char *msg, size_t len) {
+pair<string, int> std_recvfrom(int sd, vector<char> &msg) {
     struct sockaddr_in peer_addr;
     size_t peer_addr_len;
-    if(recvfrom(sd, msg, len, 0, (struct sockaddr*)&peer_addr, &peer_addr_len) < 0){
+    if(recvfrom(sd, msg.data(), msg.size(), 0, (struct sockaddr*)&peer_addr, &peer_addr_len) < 0){
         if(errno == EAGAIN or errno == EWOULDBLOCK){
-            return {"TIMEOUT", 0};
+            return {TIMEOUT_IP, 0};
         }
     }
     pair<string, int> ret;
@@ -54,84 +57,134 @@ pair<string, int> std_recvfrom(int sd, char *msg, size_t len) {
     return {string(ch), ntohs(peer_addr.sin_port)};
 }
 
-void std_sendto(int sd, char *msg, size_t len, pair<string, int> dest) {
+void std_sendto(int sd, vector<char> &msg, pair<string, int> dest) {
     struct sockaddr_in peer_addr;
     peer_addr.sin_family = AF_INET;
     peer_addr.sin_port = htons(dest.second);
     peer_addr.sin_addr.s_addr = inet_addr(dest.first.c_str());
 
-    if(sendto(sd, msg, len, 0, (struct sockaddr*)&peer_addr, sizeof(peer_addr)) < 0){}
+    if(sendto(sd, msg.data(), msg.size(), 0, (struct sockaddr*)&peer_addr, sizeof(peer_addr)) < 0){}
 }
 
 // Ignore this attempt to port
 class Sender{
-    sockaddr_in myaddr, dest;
+    pair<string, int> myaddr, dest;
     std::string fname;
+    vector<char> data;
     vector<Chunk> chunks;
-    unordered_set<int> unacked_chunks;
+    vector<bool> unacked_chunks;
     CongestionState cstate;
-    float timeoutval = 0.2;
+    int timeoutval = 200;
+    //timers if reqd
 
+public:
+    Sender(pair<string, int> myaddr_, pair<string, int> dest_, string fname_)
+    : myaddr(myaddr_), dest(dest_), fname(fname_) {
+        ifstream infile(fname, ios_base::binary);
+        data = vector<char>(istreambuf_iterator<char>(infile), istreambuf_iterator<char>());
+        populate_chunks();
+        unacked_chunks.resize(chunks.size());
+        congestion = CongestionState();
+        timeoutval = 200;
+        //timers = 
+    }
 
+    void populate_chunks(){
+        chunks.clear();
+        int seqnum = 0;
+        for(long ptr = 0; ptr < data.size();) {
+            long till = min(ptr + V1_CHUNK_SIZE, data.size());
+            vector<char> cdata(data.begin() + ptr, data.begin() + till);
+            chunks.push_back(Chunk(cdata, seqnum));
+            seqnum++;
+            ptr = till;
+        }
+    }
+
+    void do_handshake() {
+        auto mdata = Metadata(chunks.size(), fname);
+        auto pload = mdata.to_bytes();
+        mdpkt = Packet(V1_TYPE_MDATA, 0, pload);
+
+        auto sock = setup_std_sock(myaddr, timeoutval);
+
+        while(true) {
+            std_sendto(sock, mdpkt.to_bytes(), dest);
+            vector<char> msg(512);
+            auto src = std_recvfrom(sock, msg);
+            if(src.first == TIMEOUT_IP)
+                continue;
+            cout << "Received some handshake.\n";
+            auto ackpkt = Packet(msg);
+            if(ackpkt.verify_checksum() and ackpkt.type_ == V1_TYPE_MDATA_ACK)
+                break;
+        }
+        cout << "Handshake over\n";
+        close(sock);
+    }
 };
 class Sender:
-    
-    def __init__(self, myaddr:(str, int), dest:(str, int), fname:str):
-        self.myaddr = myaddr
-        self.dest:(str, int) = dest
-        self.fname:str = fname
-        with open(fname, 'rb') as file:
-            self.data:bytes = file.read()
-        self.populate_chunks()
-        self.unacked_chunks:set = set(range(len(self.chunks)))
-        self.congestion:CongestionState = CongestionState()
-        #self.flow = FlowState() TODO add flow control
-        self.timeoutval = 0.2
-        self.timers:list[threading.Timer] = [] 
 
-    def do_handshake():
-        mdata = Metadata(len(self.chunks), self.fname)
-        pload = mdata.to_bytes()
-        mdpkt = Packet.fromVals(V1_TYPE_MDATA, 0, pload)
 
-        sock = socket.socket(socket.AF_INET, sock.SOCK_DGRAM)
+    def send_data(self):    #func to start sending data, and recieving ACKs for sent data.
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.bind(self.myaddr)
-        sock.settimeout(self.timeoutval) # Initial value 0.2ms
 
-        while True:
-            try:
-                sock.sendto(mdpkt.to_bytes(), self.dest)
+        def listen_for_acks():
+            while True:
+                #print("Remaining: " + (str(len(self.unacked_chunks)))
+                #print("Remaining: " + str(np.sum(self.unacked_chunks)))
                 data, src = sock.recvfrom(512)
                 ackpkt = Packet.fromBytes(data)
-                if ackpkt.verify_checksum() and ackpkt.type == V1_TYPE_MDATA_ACK:
+                ##verify ack packet appriately
+                if ackpkt.verify_checksum() and ackpkt.type_ == V1_TYPE_DATA_ACK:
+                    ##lock here?
+                    #self.unacked_chunks.discard(ackpkt.seqnum)  ##if already acked, then does nothing
+                    self.unacked_chunks[ackpkt.seqnum] = 0
+                else:
+                    pass
+                    ##Do nothing??
+                #if len(self.unacked_chunks) == 0:
+                if np.sum(self.unacked_chunks) == 0:
                     break
 
-            except socket.timeout:
-                continue
+        thread_ACK = threading.Thread(target=listen_for_acks)
+        thread_ACK.start()
+
+        # unacked_iter = iter(self.unacked_chunks)
+        # while len(self.unacked_chunks) > 0:
+        # ##How do we put a lock here? (:
+        #     try:
+        #         ----------
+        #     except StopIteration:
+        #         unacked_iter = iter(self.unacked_chunks)
+        while np.sum(self.unacked_chunks) > 0:
+            for seq_num in range(len(self.unacked_chunks)):
+                if self.unacked_chunks[seq_num]:
+                    utp_pkt = Packet.fromVals(type_ = 1 , seqnum = seq_num, payload = self.chunks[seq_num].payload) #fromVals takes care of checksumcalculation            utp_pkt.calc_paylen()
+                    ##Checksum computed and updated
+
+                    sock.sendto(utp_pkt.to_bytes(), self.dest)
+                    ##Ig some sort of timeout here before sending again?
+
+        thread_ACK.join()
+        sock.close()
 
 
 
-    # Breaks self.data into self.chunks
-    def populate_chunks(self) -> None:
-        self.chunks = []
-        seqnum = 0
-        ptr = 0
-        while ptr < len(self.data):
-            till = math.min(ptr + V1_CHUNK_SIZE, len(self.data))
-            self.chunks.append(Chunk(data[ptr:till], seqnum))
-            seqnum += 1
-            ptr = till
 
 
     def ack_received(self):
+        pass
         # update unacked chunks
 
 class CongestionState:
     def __init__(self):
-        self.cwnd:int = 0
-        self.addconst:int = 1
-        self.ssthresh:int = 1
-        self.is_slow_start:bool = True
+        self.cwnd = 0
+        self.addconst = 1
+        self.ssthresh = 1
+        self.is_slow_start = True
     
 
 class Chunk :
@@ -139,12 +192,7 @@ class Chunk :
         self.payload = payload
         self.seq_num = seq_num
 
-def write_chunks(chunks_list, filename: str):
-    f = open(filename, "wb")
-    for chunk in chunks_list:
-        data = chunk.payload
-        f.write(data)
-    f.close()
+
         
 class Metadata :
     def __init__(self,number,name):
@@ -153,74 +201,119 @@ class Metadata :
     
     @classmethod
     def fromBytes(cls,data):
-        numchunks = int(data[0:4])
-        filename = str(data[4:504])
+        numchunks = int.from_bytes(data[0:4], 'big')
+        filename = data[4:504].decode('utf-8')
         return cls(numchunks, filename)
 
-    def tobytes(self) -> bytes:
-        return self.numchunks.to_bytes(4, 'big') + self.filename.to_bytes(500, 'big')
+    def to_bytes(self) -> bytes:
+        return self.numchunks.to_bytes(4, 'big') + self.filename.encode('utf-8')
 
-class Receiverstate :
-    def __init__(self, metadata):
-        self.metadata = metadata
+class Receiver:
+    def __init__(self, myaddr:(str, int)):
+        self.metadata = None
+        self.myaddr = myaddr
         self.chunks = []  
-        self.pending_chunks:set = set(range(len(self.chunks)))
-        self.temp_filepath
+        self.pending_chunks = set(range(len(self.chunks)))
+
+    def do_handshake(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind(self.myaddr)
+
+        while True:
+            data, self.src = sock.recvfrom(512)
+            print("recvd some handshake")
+            Headerpkt = Packet.fromBytes(data)
+            if Headerpkt.verify_checksum() and Headerpkt.type_ == V1_TYPE_MDATA:
+                ackpt = Packet.fromVals(V1_TYPE_MDATA_ACK,Headerpkt.seqnum,int(0).to_bytes(0,'big'))
+                sock.sendto(ackpt.to_bytes(),self.src)
+                break
+
+        self.metadata = Metadata.fromBytes(Headerpkt.payload)
+        self.chunks = [None for _ in range(self.metadata.numchunks)]  # Initialize list of chunks
+        self.pending_chunks = set(range(self.metadata.numchunks))  # Initialize list of pending chunks
+        print("Handshake done")
+        sock.close()
+    
+    def receive_data(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind(self.myaddr)
+
+        while len(self.pending_chunks) > 0:  # While chunks are still pending
+           # print("Remaining: " + str(len(self.pending_chunks)))
+
+            data, self.src = sock.recvfrom(512)
+            pkt = Packet.fromBytes(data)
+
+            if pkt.verify_checksum() and pkt.type_ == V1_TYPE_MDATA:  # If metadata, send mdack
+                ackpt = Packet.fromVals(V1_TYPE_MDATA_ACK, pkt.seqnum, int(0).to_bytes(0,'big'))
+                sock.sendto(ackpt.to_bytes(),self.src)
+
+            elif pkt.verify_checksum() and pkt.type_ == V1_TYPE_DATA:  # If chunk, store chunk and update pending chunks
+                seqnum = pkt.seqnum
+                self.chunks[seqnum] = Chunk(pkt.payload, seqnum)
+                ackpt = Packet.fromVals(V1_TYPE_DATA_ACK, seqnum, int(0).to_bytes(0,'big'))
+                sock.sendto(ackpt.to_bytes(),self.src)
+                self.pending_chunks.discard(seqnum)
+        sock.close()
+
+    def write_chunks(self):
+        f = open('r'+self.metadata.filename, "wb")
+        for chunk in self.chunks:
+            data = chunk.payload
+            f.write(data)
+        f.close()
 
 class Packet :
-	def __init__(self, type:int, seqnum:int, payload, payload_length:int=None, checksum:int=None):
-		self.version = 1
-		self.type_ = type_
-		self.payload_length = payload_length
-		self.checksum = checksum
-		self.seqnum = seqnum
-		self.payload = payload
+    def __init__(self, type_:int, seqnum:int, payload, payload_length:int=0, checksum:int=0):
+        self.version = 1
+        self.type_ = type_
+        self.payload_length = payload_length
+        self.checksum = checksum
+        self.seqnum = seqnum
+        self.payload = payload
 
-	@classmethod
-	def fromVals(cls, type_:int, seqnum:int, payload):  ## Creating Packet at Sender
-		pkt = cls(type_=type_, seqnum=seqnum, payload=payload)
-		pkt.calc_paylen()
-		pkt.calc_checksum()
-		return pkt
+    @classmethod
+    def fromVals(cls, type_:int, seqnum:int, payload):  ## Creating Packet at Sender
+        pkt = cls(type_=type_, seqnum=seqnum, payload=payload)
+        pkt.calc_paylen()
+        test_msg = pkt.to_bytes()
+        pkt.checksum = calc_checksum(test_msg)
+        return pkt
 
-	@classmethod
-	def fromBytes(cls, data):  ## Creating packet at Receiver
-		version = data[0] >> 4
-		type_ = (data[0] >> 2) & 3
-		payload_length = ((data[0] & 3) << 8) + data[1]
-		checksum = int(data[2:4])
-		seqnum = int(data[4:8])
-		payload = data[8:]
-		return cls(type_, seqnum, payload, payload_length, checksum)
+    @classmethod
+    def fromBytes(cls, data):  ## Creating packet at Receiver
+        version = data[0] >> 4
+        type_ = (data[0] >> 2) & 3
+        payload_length = ((data[0] & 3) << 8) + data[1]
+        checksum = int.from_bytes(data[2:4], 'big')
+        seqnum = int.from_bytes(data[4:8], 'big')
+        payload = data[8:]
+        return cls(type_, seqnum, payload, payload_length, checksum)
 
-	def to_bytes(self):
-		# 4 bits version, 2 bits type, 10 bits payload length
-		## Error handle version, type etc ranges TODO
-		first = (self.version << 28) + (self.type << 26) + 
-			(self.payload_length << 16) + self.checksum
+    def to_bytes(self):
+        # 4 bits version, 2 bits type, 10 bits payload length
+        ## Error handle version, type etc ranges TODO
+        first = (self.version << 28) + (self.type_ << 26) + (self.payload_length << 16) + self.checksum
 
-		return first.to_bytes(4, 'big') + self.seqnum.to_bytes(4, 'big') + self.payload
+        return first.to_bytes(4, 'big') + self.seqnum.to_bytes(4, 'big') + self.payload
 
-	def calc_paylen(self):
-		self.payload_length = len(self.payload)
+    def calc_paylen(self):
+        self.payload_length = len(self.payload)
 
-	#def calc_checksum(self):
-		#self.checksum = 0
-		## Calculate checksum
-		
+    def verify_checksum(self):
+        return calc_checksum(self.to_bytes()) == 0
+        ##
+
 def calc_checksum(msg:bytes):
-
-    #msg in bytes
+    #self.checksum = 0
+    ## Calculate checksum
     def add_carry(a, b):
         c = a + b
         return (c & 0xffff) + (c >> 16) #carry bit wrapped around
-
-    s = add_carry(msg[0],msg[1])
-    for i in range(2, len(msg)):
-        next_word = msg[i]
+    s = 0
+    if len(msg)%2 == 1:
+        msg = msg + int(0).to_bytes(1, 'big')
+    for i in range(0, len(msg), 2):
+        next_word = (msg[i]<<8) + msg[i+1]
         s = add_carry(s, next_word)
     return ~s & 0xffff
-
-    def verify_checksum():
-        ##
-
